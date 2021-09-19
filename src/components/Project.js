3bd65f800824c6c65491afb93776bc4735ed4ea1
Benjamin Fischer <b.fischer8708@web.de>
2021-09-19 17:16:46 +1200

    project profile iteration

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import sanityClient from '../client';
-import imageUrlBuilder from '@sanity/image-url';
-import BlockContent from '@sanity/block-content-to-react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../client";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+import ProjectNavBar from "./ProjectNavBar";
+import Contact from "./Contact";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -49,46 +51,58 @@ export default function Project() {
   console.log(singleProject);
 
   return (
-    <main className='bg-gray-200 min-h-screen p-12'>
-      <article className='container shadow-lg mx-auto bg-gray-100 rounded-lg'>
-        <header className='relative'>
-          <div className='absolute h-full w-full flex items-center justify-center p-8'>
-            <div className='bg-white bg-opacity-75 rounded p-12'>
-              <h1 className='text-3xl lg:text-6xl mb-4'>
-                {singleProject.title}
-              </h1>
-              <div className='flex justify-center text-gray-800'>
-                {singleProject.students &&
-                  singleProject.students.map((student) => (
-                    <div>
-                      <img
-                        src={urlFor(student.mainImage).url()}
-                        alt={student.name}
-                        className='w-10 h-10 rounded-full'
-                      />
-                      <p className='flex items-center pl-2 text-2xl'>
-                        {student.name}
-                      </p>
-                    </div>
-                  ))}
-              </div>
-            </div>
-          </div>
-          <img
-            src={singleProject.mainImage.asset.url}
-            alt={singleProject.title}
-            className='w-full object-cover rounded-t'
-            style={{ height: '400px' }}
-          />
-        </header>
-        <div className='px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full'>
-          <BlockContent
-            blocks={singleProject.body}
-            projectId='x44gmgik'
-            dataset='production'
-          />
-        </div>
-      </article>
+    // <main className='bg-gray-200 min-h-screen p-12'>
+    //   <article className='container shadow-lg mx-auto bg-gray-100 rounded-lg'>
+    //     <header className='relative'>
+    //       <div className='absolute h-full w-full flex items-center justify-center p-8'>
+    //         <div className='bg-white bg-opacity-75 rounded p-12'>
+    //           <h1 className='text-3xl lg:text-6xl mb-4'>
+    //             {singleProject.title}
+    //           </h1>
+    //         </div>
+    //       </div>
+    //       <img
+    //         src={singleProject.mainImage.asset.url}
+    //         alt={singleProject.title}
+    //         className='w-full object-cover rounded-t'
+    //         style={{ height: '400px' }}
+    //       />
+    //     </header>
+    //     <div className='px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full'>
+    //       <BlockContent
+    //         blocks={singleProject.body}
+    //         projectId='x44gmgik'
+    //         dataset='production'
+    //       />
+    //     </div>
+    //   </article>
+    //   <div className='flex justify-center text-gray-800'>
+    //             {singleProject.students &&
+    //               singleProject.students.map((student) => (
+    //                 <div>
+    //                   <img
+    //                     src={urlFor(student.mainImage).url()}
+    //                     alt={student.name}
+    //                     className='w-10 h-10 rounded-full'
+    //                   />
+    //                   <p className='flex items-center pl-2 text-2xl'>
+    //                     {student.name}
+    //                   </p>
+    //                 </div>
+    //               ))}
+    //           </div>
+    // </main>
+    <main className="bg-gray-100">
+      <ProjectNavBar />
+      <div className="min-h-screen mx-auto">
+        <img
+          src={singleProject.mainImage.asset.url}
+          alt={singleProject.title}
+          className="w-full object-cover "
+          style={{ height: "400px" }}
+        />
+      </div>
+      <Contact />
     </main>
   );
 }
---
+++ src/components/ProjectNavBar.js
@@ -0,0 +1,227 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Link as SLink } from "react-scroll";
+import * as Unicons from "@iconscout/react-unicons";
+
+export default function ProjectNavBar() {
+  const [navbar, setNavbar] = useState(false);
+
+  const [mobileButton, setMobileButton] = useState(false);
+  const [mobileNav, setMobileNav] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 350) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  const homeClick = () => {
+    setNavbar(false);
+    setMobileButton(false);
+    setMobileNav(false);
+  };
+
+  const mobileNavBackground = () => {
+    if (window.scrollY <= 300) {
+      setMobileNav(!mobileButton);
+    }
+  };
+
+  const eventHandler = () => {
+    console.log("mobile clicked");
+    setMobileButton(!mobileButton);
+    mobileNavBackground();
+  };
+
+  window.addEventListener("scroll", changeBackground);
+
+  return (
+    <nav
+      className={
+        navbar || mobileNav
+          ? " right-0 left-0 inset-x-0 .bg-project-nav.activeback  z-50 activeback section.open "
+          : "right-0 left-0 inset-x-0 .bg-project-nav.activeback  z-50 section"
+      }
+    >
+      <section className="container mx-auto text-gray-700">
+        <div className="sm:self-end grid md:grid-cols-1 items-center lg:grid-cols-2 gap-2">
+          <div className="hidden md:flex items-center font-trueno mr-10">
+            <h2 className="text-4xl logo  ">TECH COLAB / </h2>
+            <p className="text-xs font-thin logo-add p-4 mr-10">
+              We are Creative
+            </p>
+            {/* MOBILE MENU */}
+          </div>
+          <div className="md:hidden py-8 px-3">
+            <div className=" md:hidden flex justify-between">
+              <div className="w-30">
+                {" "}
+                <h4 className="text-2xl logo pt-1  w-max">TECH COLAB </h4>
+              </div>
+              <button
+                className="mobile-menu-button text-4xl"
+                onClick={eventHandler}
+              >
+                {" "}
+                <ion-icon name="menu-outline"></ion-icon>
+              </button>
+            </div>
+          </div>
+          <div className="hidden lg:flex justify-end md:flex test-query ">
+            <SLink
+              className="py-5 px-4 cursor-pointer hover:underline"
+              activeClass="active"
+              to="welcome"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
+            >
+              HOME
+            </SLink>
+            <SLink
+              className="py-5 px-4 cursor-pointer hover:underline"
+              activeClass="active"
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={-60}
+              duration={1000}
+            >
+              ABOUT
+            </SLink>
+            <SLink
+              className="py-5 px-4 cursor-pointer hover:underline"
+              activeClass="active"
+              to="project"
+              spy={true}
+              smooth={true}
+              offset={-55}
+              duration={1000}
+            >
+              PROJECTS
+            </SLink>
+            <SLink
+              className="py-5 px-4 cursor-pointer hover:underline"
+              activeClass="active"
+              to="student"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={1000}
+            >
+              STUDENTS
+            </SLink>
+
+            <SLink
+              className="py-5 px-4 cursor-pointer hover:underline"
+              activeClass="active"
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
+            >
+              CONTACT
+            </SLink>
+            <SLink
+              className="py-5 px-4 cursor-pointer hover:underline"
+              activeClass="active"
+              to="navbar"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={1000}
+            >
+              |
+            </SLink>
+            <Link
+              className="py-5 pl-5 hover:underline"
+              to={{ pathname: "https://www.ara.ac.nz" }}
+              target="_blank"
+            >
+              ARA
+            </Link>
+          </div>
+
+          {/* mobile menu */}
+          {mobileButton && (
+            <div className="flex flex-col justify-center">
+              <SLink
+                onClick={homeClick}
+                className="py-5 px-4 cursor-pointer hover:underline mx-auto"
+                activeClass="active"
+                to="welcome"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={1000}
+              >
+                HOME
+              </SLink>
+              <SLink
+                onClick={() => setMobileButton(!mobileButton)}
+                className="py-5 px-4 cursor-pointer hover:underline  mx-auto"
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={1000}
+              >
+                ABOUT
+              </SLink>
+              <SLink
+                onClick={() => setMobileButton(!mobileButton)}
+                className="py-5 px-4 cursor-pointer hover:underline  mx-auto"
+                activeClass="active"
+                to="project"
+                spy={true}
+                smooth={true}
+                offset={-55}
+                duration={1000}
+              >
+                PROJECTS
+              </SLink>
+              <SLink
+                onClick={() => setMobileButton(!mobileButton)}
+                className="py-5 px-4 cursor-pointer hover:underline  mx-auto"
+                activeClass="active"
+                to="student"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={1000}
+              >
+                STUDENTS
+              </SLink>
+
+              <SLink
+                onClick={() => setMobileButton(!mobileButton)}
+                className="py-5 px-4 cursor-pointer hover:underline  mx-auto"
+                activeClass="active"
+                to="contact"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={1000}
+              >
+                CONTACT
+              </SLink>
+              <Link
+                onClick={() => setMobileButton(!mobileButton)}
+                className="py-5 pl-5 hover:underline  mx-auto sm: py-5 px-5"
+                to={{ pathname: "https://www.ara.ac.nz" }}
+                target="_blank"
+              >
+                ARA
+              </Link>
+            </div>
+          )}
+        </div>
+      </section>
+    </nav>
+  );
+}
