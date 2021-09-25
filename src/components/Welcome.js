edf95162ff54599fc92970992970c0c13f471d2b
Benjamin Fischer <b.fischer8708@web.de>
2021-09-25 18:08:49 +1200

    mobile background added

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,45 @@
 import sample from '../_images/OMGBG4.mov';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link as SLink } from 'react-scroll';
+
 export default function Welcome() {
+  const [mobile, setMobile] = useState(false);
+
+  const changeBackground = () => {
+    if (window.innerWidth < 750) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  };
+
+  window.addEventListener('resize', changeBackground);
+
   return (
     <div className='min-h-screen object-cover'>
-      <div className='flex justify-between min-h-screen object-cover' id='welcome'>
-        <video autoPlay loop muted className='z-30 object-cover'>
-          <source className='z-30 min-h-screen object-cover' src={sample} type='video/mp4' />
-        </video>
-      </div>
+      {!mobile ? (
+        <div>
+          <div
+            className='flex justify-between min-h-screen object-cover'
+            id='welcome'
+          >
+            <video autoPlay loop muted className='z-30 object-cover'>
+              <source
+                className='z-30 min-h-screen object-cover'
+                src={sample}
+                type='video/mp4'
+              />
+            </video>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div
+            className='flex justify-between min-h-screen object-cover const-bg'
+            id='welcome'
+          ></div>
+        </div>
+      )}
       {/* BUTTON ARROW*/}
       <SLink
         className=' cursor-pointer hover:underline'
@@ -17,13 +48,15 @@ export default function Welcome() {
         spy={true}
         smooth={true}
         offset={-70}
-        duration={1000}>
+        duration={1000}
+      >
         {' '}
         <div className='invisible md:visible animate-bounce text-5xl margin-set absolute -mt-52 z-50 '>
           <ion-icon
             className=''
             id='ion-icon-arrow'
-            name='arrow-down-circle-outline'></ion-icon>
+            name='arrow-down-circle-outline'
+          ></ion-icon>
         </div>
       </SLink>
     </div>
