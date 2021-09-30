8b7cfc51517045a2050eaa245d3db50f42c84ed0
Benjamin Fischer <b.fischer8708@web.de>
2021-09-30 20:12:05 +1300

    type effect welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,9 +3,85 @@ import React from 'react';
 import { Link as SLink } from 'react-scroll';
 
 export default function Welcome() {
+  class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+      this.txtElement = txtElement;
+      this.words = words;
+      this.txt = '';
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 10);
+      this.type();
+      this.isDeleting = false;
+    }
+
+    type() {
+      // Current index of word
+      const current = this.wordIndex % this.words.length;
+      // Get full text of current word
+      const fullTxt = this.words[current];
+
+      // Check if deleting
+      if (this.isDeleting) {
+        // Remove char
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        // Add char
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      // Insert txt into element
+      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+      // Initial Type Speed
+      let typeSpeed = 300;
+
+      if (this.isDeleting) {
+        typeSpeed /= 2;
+      }
+
+      // If word is complete
+      if (!this.isDeleting && this.txt === fullTxt) {
+        // Make pause at end
+        typeSpeed = this.wait;
+        // Set delete to true
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        // Move to next word
+        this.wordIndex++;
+        // Pause before start typing
+        typeSpeed = 500;
+      }
+
+      setTimeout(() => this.type(), typeSpeed);
+    }
+  }
+
+  // Init On DOM Load
+  document.addEventListener('DOMContentLoaded', init);
+
+  // Init App
+  function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+  }
   return (
     <div className='min-h-screen object-cover '>
-      {/* {!mobile ? ( */}
+      <div className='container mx-auto'>
+        <div className='text-5xl absolute bg-transparent z-50 h-52 pt-72 '>
+          <p>
+            Your Software
+            <span
+              className='txt-type'
+              data-wait='3000'
+              data-words='[" Partner", " Designer", " Inventor", " Creator", " Team"]'
+            ></span>
+          </p>
+        </div>
+      </div>
       <div className='media-desktop'>
         <div
           className='flex justify-between min-h-screen object-cover'
